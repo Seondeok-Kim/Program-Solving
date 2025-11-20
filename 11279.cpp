10a23053ff4d18a9d0e10a77ead7e5b8e8a868ff
seon deok Kim <76850241+Seondeok-Kim@users.noreply.github.com>
2025-11-20 23:11:39 +0900

    Create 11279.cpp

---
+++ 11279.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    priority_queue<int> pq; // 최대 힙
+
+    while (N--) {
+        int x;
+        cin >> x;
+
+        if (x == 0) {
+            if (pq.empty()) {
+                cout << 0 << '\n';
+            } else {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        } else {
+            pq.push(x);
+        }
+    }
+    return 0;
+}
