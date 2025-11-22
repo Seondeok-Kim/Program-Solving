7da961c574c94378e42102a7ffd0000e1323355e
seon deok Kim <76850241+Seondeok-Kim@users.noreply.github.com>
2025-11-23 03:51:11 +0900

    Add files via upload

---
+++ Sorting/1181.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+bool cmp(const string &a, const string &b)
+{
+    if (a.length() == b.length())
+    {
+        return a < b;
+    }
+    else
+    {
+        return a.length() < b.length();
+    }
+
+}
+
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int N;
+    cin >> N;
+
+    vector<string> v(N);
+    
+    for(int i = 0 ; i < N ; ++i)
+    {
+        cin >> v[i];
+    }
+    
+    sort(v.begin(), v.end(), cmp);
+    v.erase(unique(v.begin(), v.end()), v.end());
+    
+    for(string s : v)
+    {
+        cout << s << endl;
+    }
+	return 0;
+}
