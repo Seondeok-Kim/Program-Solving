c5ef73183e5c890eae88c462d8683008c68d30fe
seon deok Kim <76850241+Seondeok-Kim@users.noreply.github.com>
2025-11-23 20:33:49 +0900

    Create 11651.cpp

---
+++ Sorting/11651.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool cmp(const pair<int,int> &a, const pair<int, int> &b)
+{   
+    if(a.second == b.second)
+    {
+        return a.first < b.first;
+    }    
+    return a.second < b.second;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int N;
+    cin >> N;
+    vector<pair<int, int>> v(N);
+    
+
+    for(int i = 0 ; i < N ; ++i)
+    {
+        cin >> v[i].first >> v[i].second ;
+    }
+    
+    sort(v.begin(), v.end(), cmp);
+
+        for(int i = 0 ; i < N ; ++i)
+    {
+        cout << v[i].first << " " << v[i].second << endl ;
+    }
+    
+	return 0;
+}
